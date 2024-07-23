048805ad61562d4253e471eea99fe8169be51227
Joakim-animate90 <joakimbwire23@gmail.com>
2024-07-24 00:25:12 +0300

    Addition of payment configuration file

--- service/business/payment_test.go
+++ service/business/payment_test.go
@@ -2,13 +2,79 @@ package business
 
 import (
 	"context"
+	"fmt"
 	"github.com/antinvestor/apis/go/common"
 	partitionV1 "github.com/antinvestor/apis/go/partition/v1"
 	profileV1 "github.com/antinvestor/apis/go/profile/v1"
+
+	"github.com/antinvestor/service-payments-v1/service/events"
 	"github.com/pitabwire/frame"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 	"go.uber.org/mock/gomock"
 	"testing"
+	"time"
 )
+
+func getService(serviceName string) (*ctxSrv, error) {
+	ctx := context.Background()
+
+	req := testcontainers.ContainerRequest{
+		Image:        "postgres:latest",
+		ExposedPorts: []string{"5432/tcp"},
+		Env: map[string]string{
+			"POSTGRES_USER":     "ant",
+			"POSTGRES_PASSWORD": "secret",
+			"POSTGRES_DB":       "service_payment",
+		},
+		WaitingFor: wait.ForListeningPort("5432/tcp").WithStartupTimeout(5 * time.Minute),
+	}
+
+	postgresC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to start container: %w", err)
+	}
+
+	mappedPort, err := postgresC.MappedPort(ctx, "5432")
+	if err != nil {
+		return nil, fmt.Errorf("failed to get mapped port: %w", err)
+	}
+
+	hostIP, err := postgresC.Host(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get container host: %w", err)
+	}
+
+	dbURL := fmt.Sprintf("postgres://ant:secret@%s:%s/service_notification?sslmode=disable", hostIP, mappedPort.Port())
+	testDb := frame.DatastoreCon(dbURL, false)
+
+	var ncfg config.NotificationConfig
+	_ = frame.ConfigProcess("", &ncfg)
+
+	ctx, service := frame.NewService(serviceName, testDb, frame.Config(&ncfg), frame.NoopDriver())
+
+	m := make(map[string]string)
+	m["sub"] = "testing"
+	m["tenant_id"] = "test_tenant-id"
+	m["partition_id"] = "test_partition-id"
+	m["access_id"] = "test_access-id"
+
+	//claims := frame.ClaimsFromMap(m)
+	//ctx = claims.ClaimsToContext(ctx)
+
+	eventList := frame.RegisterEvents(
+		&events.PaymentSave{Service: service},
+		&events.PaymentStatusSave{Service: service})
+	service.Init(eventList)
+	_ = service.Run(ctx, "")
+	return &ctxSrv{
+		ctx,
+		service,
+	}, nil
+}
 
 type ctxSrv struct {
 	ctx context.Context
---
+++ service/config/config.go
@@ -0,0 +1,9 @@
+package config
+
+import "github.com/pitabwire/frame"
+
+type PaymentConfig struct {
+	frame.ConfigurationDefault
+	ProfileServiceURI   string `default:"127.0.0.1:7005" envconfig:"PROFILE_SERVICE_URI"`
+	PartitionServiceURI string `default:"127.0.0.1:7003" envconfig:"PARTITION_SERVICE_URI"`
+}

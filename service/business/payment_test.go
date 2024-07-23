6f07fd4e4c8e3fb2c5419d27b6f68cd0334dab0b
Joakim-animate90 <joakimbwire23@gmail.com>
2024-07-23 23:36:30 +0300

    Add tests on payments

--- service/business/payment_test.go
+++ service/business/payment_test.go
@@ -1,1 +1,63 @@
 package business
+
+import (
+	"context"
+	"github.com/antinvestor/apis/go/common"
+	partitionV1 "github.com/antinvestor/apis/go/partition/v1"
+	profileV1 "github.com/antinvestor/apis/go/profile/v1"
+	"github.com/pitabwire/frame"
+	"go.uber.org/mock/gomock"
+	"testing"
+)
+
+type ctxSrv struct {
+	ctx context.Context
+	srv *frame.Service
+}
+
+func getProfileCli(t *testing.T) *profileV1.ProfileClient {
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockProfileService := profileV1.NewMockProfileServiceClient(ctrl)
+	mockProfileService.EXPECT().
+		GetById(gomock.Any(), gomock.Any()).
+		Return(&profileV1.GetByIdResponse{
+			Data: &profileV1.ProfileObject{
+				Id: "test_profile-id",
+			},
+		}, nil).AnyTimes()
+	mockProfileService.EXPECT().
+		GetByContact(gomock.Any(), gomock.Any()).
+		Return(&profileV1.GetByContactResponse{
+			Data: &profileV1.ProfileObject{
+				Id: "test_profile-id",
+			},
+		}, nil).AnyTimes()
+
+	profileCli := profileV1.Init(&common.GrpcClientBase{}, mockProfileService)
+	return profileCli
+}
+
+func getPartitionCli(t *testing.T) *partitionV1.PartitionClient {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockPartitionService := partitionV1.NewMockPartitionServiceClient(ctrl)
+
+	mockPartitionService.EXPECT().
+		GetAccess(gomock.Any(), gomock.Any()).
+		Return(&partitionV1.GetAccessResponse{Data: &partitionV1.AccessObject{
+			AccessId: "test_access-id",
+			Partition: &partitionV1.PartitionObject{
+				Id:       "test_partition-id",
+				TenantId: "test_tenant-id",
+			},
+		}}, nil).AnyTimes()
+
+	profileCli := partitionV1.Init(&common.GrpcClientBase{}, mockPartitionService)
+	return profileCli
+}
+
+func TestNewPaymentBusiness(t *testing.T) {
+
+}

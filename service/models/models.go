0b87615f9813af070b0ed0ee381b15319aabf2c8
Joakim-animate90 <joakimbwire23@gmail.com>
2024-08-20 18:27:37 +0300

    changes in models

--- service/models/models.go
+++ service/models/models.go
@@ -1,8 +1,6 @@
 package models
 
 import (
-	"time"
-
 	commonv1 "github.com/antinvestor/apis/go/common/v1"
 	paymentV1 "github.com/antinvestor/apis/go/payment/v1"
 	"github.com/pitabwire/frame"
@@ -23,28 +21,15 @@ type Payment struct {
 	RecipientProfileType string `gorm:"type:varchar(50)"`
 	RecipientContactID   string `gorm:"type:varchar(50)"`
 
-	Id                    string `gorm:"type:varchar(50)"`
-	TransactionId         string `gorm:"type:varchar(50)"`
-	ReferenceId           string `gorm:"type:varchar(50)"`
-	BatchId               string `gorm:"type:varchar(50)"`
-	ExternalTransactionId string `gorm:"type:varchar(50)"`
-	Route                 string `gorm:"type:varchar(50)"`
-
-	Source        *commonv1.ContactLink
-	Recipient     *commonv1.ContactLink
 	Amount        decimal.NullDecimal `gorm:"type:numeric" json:"amount"`
-	Cost          decimal.NullDecimal `gorm:"type:numeric" json:"cost"`
-	Currency      string              `gorm:"type:varchar(10)" json:"currency"`
-	State         commonv1.STATE
-	Status        commonv1.STATUS
-	DateCreated   *time.Time
-	DateProcessed *time.Time
-	Outbound      bool
-	Extra         datatypes.JSONMap
-}
+	TransactionId string              `gorm:"type:varchar(50)"`
+	ReferenceId   string              `gorm:"type:varchar(50)"`
+	BatchId       string              `gorm:"type:varchar(50)"`
+	Route         string              `gorm:"type:varchar(50)"`
+	Currency      string              `gorm:"type:varchar(10)"`
 
-func (model *Payment) IsProcessed() bool {
-	return model.DateProcessed != nil && !model.DateProcessed.IsZero()
+	Outbound bool
+	Extra    datatypes.JSONMap `gorm:"index:,type:gin,option:jsonb_path_ops" json:"extra"`
 }
 
 func (model *Payment) ToApi() *paymentV1.Payment {
@@ -53,10 +38,6 @@ func (model *Payment) ToApi() *paymentV1.Payment {
 	extra["tenant_id"] = model.TenantID
 	extra["partition_id"] = model.PartitionID
 	extra["access_id"] = model.AccessID
-
-	if model.IsProcessed() {
-		extra["ProcessedDate"] = model.DateProcessed.String()
-	}
 
 	source := &commonv1.ContactLink{
 		ProfileType: model.SenderProfileType,
@@ -71,25 +52,27 @@ func (model *Payment) ToApi() *paymentV1.Payment {
 	}
 
 	payment := paymentV1.Payment{
-		Id:                    model.ID,
-		Source:                source,
-		Recipient:             recipient,
-		Amount:                &money.Money{CurrencyCode: model.Currency, Units: model.Amount.Decimal.CoefficientInt64()},
-		Cost:                  &money.Money{CurrencyCode: model.Currency, Units: model.Cost.Decimal.CoefficientInt64()},
-		State:                 model.State,
-		Status:                model.Status,
-		TransactionId:         model.TransactionId,
-		ReferenceId:           model.ReferenceId,
-		BatchId:               model.BatchId,
-		ExternalTransactionId: model.ExternalTransactionId,
-		Route:                 model.Route,
-		Outbound:              model.Outbound,
-		DateCreated:           model.DateCreated.Format(time.RFC3339),
-		DateProcessed:         model.DateProcessed.Format(time.RFC3339),
-		Extra:                 extra,
+		Id:            model.ID,
+		Source:        source,
+		Recipient:     recipient,
+		Amount:        &money.Money{CurrencyCode: model.Currency, Units: model.Amount.Decimal.CoefficientInt64()},
+		TransactionId: model.TransactionId,
+		ReferenceId:   model.ReferenceId,
+		BatchId:       model.BatchId,
+		Route:         model.Route,
+		Outbound:      model.Outbound,
+		Extra:         extra,
 	}
 
 	return &payment
+}
+
+type Cost struct {
+	frame.BaseModel
+	PaymentID string              `gorm:"type:varchar(50)"`
+	Amount    decimal.NullDecimal `gorm:"type:numeric" json:"amount"`
+	Currency  string
+	Extra     datatypes.JSONMap
 }
 
 type PaymentStatus struct {

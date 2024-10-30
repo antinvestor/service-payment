d1d2cfd65785cbd860b6e0bffa9d69a6d4ac72fb
Joakim-animate90 <joakimbwire23@gmail.com>
2024-10-30 17:29:34 +0300

    ```text Refactor jenga-api client package and add new functionalities
    This commit refactors the jenga-api client package and adds new functionalities to the jenga api. The changes improve security and code organization.

--- integrations/jenga-api/coreapi/client.go
+++ integrations/jenga-api/coreapi/client.go
@@ -74,33 +74,28 @@ func (c *Client) GenerateBearerToken() (*BearerTokenResponse, error) {
 	return &tokenResponse, nil
 }
 
-// SendMobileRequest represents the request structure for sending money to mobile wallets
-type SendMobileRequest struct {
-	Source struct {
+
+// STKUSSDRequest represents the structure for the STK/USSD push request
+type STKUSSDRequest struct {
+	Merchant struct {
 		CountryCode   string `json:"countryCode"`
-		Name          string `json:"name"`
 		AccountNumber string `json:"accountNumber"`
-	} `json:"source"`
-	Destination struct {
-		Type         string `json:"type"`
-		CountryCode  string `json:"countryCode"`
-		Name         string `json:"name"`
+		Name          string `json:"name"`
+	} `json:"merchant"`
+	Payment struct {
+		Ref         string `json:"ref"`
+		Amount      string `json:"amount"`
+		Currency    string `json:"currency"`
+		Telco       string `json:"telco"`
 		MobileNumber string `json:"mobileNumber"`
-		WalletName   string `json:"walletName"`
-	} `json:"destination"`
-	Transfer struct {
-		Type         string `json:"type"`
-		Amount       string `json:"amount"`
-		CurrencyCode string `json:"currencyCode"`
-		Reference    string `json:"reference"`
-		Date         string `json:"date"`
-		Description  string `json:"description"`
-		CallbackURL  string `json:"callbackUrl"`
-	} `json:"transfer"`
+		Date        string `json:"date"`
+		CallBackUrl string `json:"callBackUrl"`
+		PushType    string `json:"pushType"`
+	} `json:"payment"`
 }
 
-// SendMobileResponse represents the response structure for a mobile wallet transaction
-type SendMobileResponse struct {
+// STKUSSDResponse represents the response structure for the STK/USSD push initiation
+type STKUSSDResponse struct {
 	Status        bool   `json:"status"`
 	Code          int    `json:"code"`
 	Message       string `json:"message"`
@@ -108,139 +103,55 @@ type SendMobileResponse struct {
 	TransactionID string `json:"transactionId"`
 }
 
-// SendMobile sends money to a mobile wallet
-func (c *Client) SendMobile(req *SendMobileRequest, token string) (*SendMobileResponse, error) {
-	url := fmt.Sprintf("%s/v3-apis/transaction-api/v3.0/remittance/sendmobile", c.Env) // Adjust to live URL for production
+// GenerateSignature generates a HMAC-SHA256 signature for the STK/USSD push request
+func (c *Client) GenerateSignature(accountNumber, ref, mobileNumber, telco, amount, currency string) string {
+	data := accountNumber + ref + mobileNumber + telco + amount + currency
+	mac := hmac.New(sha256.New, []byte(c.ConsumerSecret))
+	mac.Write([]byte(data))
+	return hex.EncodeToString(mac.Sum(nil))
+}
 
-	// Create the signature
-	signature := c.generateSignature(req)
+// InitiateSTKUSSD initiates an STK/USSD push request
+func (c *Client) InitiateSTKUSSD(request STKUSSDRequest, accessToken string) (*STKUSSDResponse, error) {
+	url := fmt.Sprintf("%s/v3-apis/payment-api/v3.0/stkussdpush/initiate", c.Env)
 
-	jsonBody, err := json.Marshal(req)
+	// Generate the signature for the request
+	signature := c.GenerateSignature(
+		request.Merchant.AccountNumber,
+		request.Payment.Ref,
+		request.Payment.MobileNumber,
+		request.Payment.Telco,
+		request.Payment.Amount,
+		request.Payment.Currency,
+	)
+
+	jsonBody, err := json.Marshal(request)
 	if err != nil {
 		return nil, err
 	}
 
-	httpReq, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(jsonBody))
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(jsonBody))
 	if err != nil {
 		return nil, err
 	}
-	httpReq.Header.Set("Content-Type", "application/json")
-	httpReq.Header.Set("Authorization", "Bearer "+token)
-	httpReq.Header.Set("signature", signature)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+accessToken)
+	req.Header.Set("Signature", signature)
 
-	resp, err := c.HttpClient.Do(httpReq)
+	resp, err := c.HttpClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("failed to send mobile payment: %s", resp.Status)
+		return nil, fmt.Errorf("failed to initiate STK/USSD push: %s", resp.Status)
 	}
 
-	var sendMobileResponse SendMobileResponse
-	if err := json.NewDecoder(resp.Body).Decode(&sendMobileResponse); err != nil {
+	var stkUssdResponse STKUSSDResponse
+	if err := json.NewDecoder(resp.Body).Decode(&stkUssdResponse); err != nil {
 		return nil, err
 	}
-	return &sendMobileResponse, nil
+	return &stkUssdResponse, nil
 }
 
-// InternalBankTransferRequest represents the request structure for internal bank transfer
-type InternalBankTransferRequest struct {
-	Source struct {
-		CountryCode   string `json:"countryCode"`
-		Name          string `json:"name"`
-		AccountNumber string `json:"accountNumber"`
-	} `json:"source"`
-	Destination struct {
-		Type         string `json:"type"`
-		CountryCode  string `json:"countryCode"`
-		Name         string `json:"name"`
-		AccountNumber string `json:"accountNumber"`
-	} `json:"destination"`
-	Transfer struct {
-		Type         string `json:"type"`
-		Amount       string `json:"amount"`
-		CurrencyCode string `json:"currencyCode"`
-		Reference    string `json:"reference"`
-		Date         string `json:"date"`
-		Description  string `json:"description"`
-	} `json:"transfer"`
-}
-
-// InternalBankTransferResponse represents the response structure for an internal bank transfer
-type InternalBankTransferResponse struct {
-	Status        bool   `json:"status"`
-	Code          int    `json:"code"`
-	Message       string `json:"message"`
-	Reference     string `json:"reference"`
-	Data          struct {
-		TransactionID string `json:"transactionId"`
-		Status        string `json:"status"`
-	} `json:"data"`
-}
-
-// InternalBankTransfer performs an internal bank transfer within Equity Bank
-func (c *Client) InternalBankTransfer(req *InternalBankTransferRequest, token string) (*InternalBankTransferResponse, error) {
-	url := fmt.Sprintf("%s/v3-apis/transaction-api/v3.0/remittance/internalBankTransfer", c.Env) // Adjust to live URL for production
-
-	// Create the signature
-	signature := c.generateInternalTransferSignature(req)
-
-	jsonBody, err := json.Marshal(req)
-	if err != nil {
-		return nil, err
-	}
-
-	httpReq, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(jsonBody))
-	if err != nil {
-		return nil, err
-	}
-	httpReq.Header.Set("Content-Type", "application/json")
-	httpReq.Header.Set("Authorization", "Bearer "+token)
-	httpReq.Header.Set("signature", signature)
-
-	resp, err := c.HttpClient.Do(httpReq)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("failed to perform internal bank transfer: %s", resp.Status)
-	}
-
-	var internalBankTransferResponse InternalBankTransferResponse
-	if err := json.NewDecoder(resp.Body).Decode(&internalBankTransferResponse); err != nil {
-		return nil, err
-	}
-	return &internalBankTransferResponse, nil
-}
-
-// generateSignature generates the required signature for the mobile payment request
-func (c *Client) generateSignature(req *SendMobileRequest) string {
-	signatureData := fmt.Sprintf("%s%s%s%s",
-		req.Transfer.Amount,
-		req.Transfer.CurrencyCode,
-		req.Transfer.Reference,
-		req.Source.AccountNumber,
-	)
-
-	h := hmac.New(sha256.New, []byte(c.ConsumerSecret))
-	h.Write([]byte(signatureData))
-	return fmt.Sprintf("%x", h.Sum(nil))
-}
-
-// generateInternalTransferSignature generates the required signature for the internal bank transfer request
-func (c *Client) generateInternalTransferSignature(req *InternalBankTransferRequest) string {
-	signatureData := fmt.Sprintf("%s%s%s%s",
-		req.Transfer.Amount,
-		req.Transfer.CurrencyCode,
-		req.Transfer.Reference,
-		req.Source.AccountNumber,
-	)
-
-	h := hmac.New(sha256.New, []byte(c.ConsumerSecret))
-	h.Write([]byte(signatureData))
-	return fmt.Sprintf("%x", h.Sum(nil))
-}

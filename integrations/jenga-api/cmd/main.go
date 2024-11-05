3a7340bb7d09db204cec8be988880ebb6160b03b
Joakim-animate90 <joakimbwire23@gmail.com>
2024-11-05 18:08:53 +0300

    Addition of main file

--- integrations/jenga-api/cmd/main.go
+++ integrations/jenga-api/cmd/main.go
@@ -1,1 +1,13 @@
 package cmd
+
+import (
+	"github.com/antinvestor/service-payments/integrations/jenga-api/service/router"
+	"net/http"
+)
+
+//our main application
+
+func main() {
+	router := router.NewRouter()
+	http.ListenAndServe(":8080", router)
+}
